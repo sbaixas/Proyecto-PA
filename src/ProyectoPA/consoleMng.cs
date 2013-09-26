a30a257a0f86d4a2ae67405a6ee2f5f08ff1be92
Sebastian Bozzo <sjbozzo@miuandes.cl>
2013-09-26 00:07:43 -0300

    bozzo: agrego el console mng , que servira como explorer.exe

---
+++ src/ProyectoPA/consoleMng.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPA
+{
+    class consoleMng
+    {
+        //Atributos//
+        private optionTree current;
+        private static int id;
+
+        //Getters y Setters//
+        public static int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        //Contructor//
+        public consoleMng()
+        {
+            optionTree raiz = new optionTree("main", null, Id);
+            current = raiz;
+            raiz.agregarHijo("primeraOpcion");
+            raiz.agregarHijo("SegundaOpcion");
+            raiz.agregarHijo("TerceraOpcion");
+            raiz.agregarHijo("Salir");
+            navegar();
+
+        }
+
+        //Metodos//
+        public int seleccionar()
+        {
+            bool correcto = false;
+            int retorno = -2;
+            while (!correcto)
+            {
+                string input = Console.ReadLine();
+                if (input != "*")
+                {
+                    try
+                    {
+                        int opcion = Convert.ToInt32(input);
+                        if (0 < opcion && opcion < current.SubOptions.Length)
+                        {
+                            retorno = opcion -1;
+                            correcto = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("La Opcion Seleccionada no existe , presione ENTER para continuar");
+
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Has ingresado mal , presione ENTER para continuar");
+
+                    }
+                }
+
+                else
+                {
+                    correcto = true;
+                    retorno = -1;
+                }
+            }
+            Console.WriteLine(retorno);
+
+            return retorno;
+        }
+
+        public void navegar()
+        {
+            int seleccion;
+            while (true)
+            {
+                current.show();
+                seleccion = seleccionar();
+
+
+                if (seleccion == -1)
+                {
+                    current = current.BackOption;
+                }
+                else
+                {
+                    if (!current.SubOptions[seleccion].Exe)
+                    {
+                        current = current.SubOptions[seleccion];
+                    }
+                    else
+                    {
+                        //ejecutar//
+                    }
+                }
+                
+            }
+        }
+        public void mostrarTodadLasOpciones()
+        {
+            Console.Clear();
+            Console.WriteLine("nombre: " + current.Titulo);
+            Console.WriteLine("option Id " + current.OptionId + "");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            optionTree aux = current;
+            for (int i = 0; i < aux.SubOptions.Length; i++)
+            {
+
+                optionTree aux2 = aux.SubOptions[i];
+
+                for (int j = 0; j < aux2.SubOptions.Length; j++)
+                {
+                    Console.WriteLine("nombre: " + aux2.SubOptions[j].Titulo);
+                    Console.WriteLine("option Id " + aux2.SubOptions[j].OptionId + "");
+                    Console.WriteLine("");
+                }
+                Console.WriteLine("-------------------------------------------------------------------");
+            }
+
+        }
+    }
+}
---
+++ src/ProyectoPA/optionTree.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPA
+{
+    class optionTree
+    {
+        //atributos//
+        private optionTree[] subOptions;
+        private string titulo;
+        private int optionId;
+        private optionTree backOption;
+        private bool exe;
+
+
+
+        //Getters y Setters//
+        public optionTree[] SubOptions
+        {
+            get { return subOptions; }
+            set { subOptions = value; }
+        }
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+        public int OptionId
+        {
+            get { return optionId; }
+            set { optionId = value; }
+        }
+        public optionTree BackOption
+        {
+            get { return backOption; }
+            set { backOption = value; }
+        }
+        public bool Exe
+        {
+            get { return exe; }
+            set { exe = value; }
+        }
+
+        //Constructor//
+        public optionTree(string titulo, optionTree padre, int id)
+        {
+            exe = false;
+            optionId = consoleMng.Id;
+            consoleMng.Id++;
+            this.titulo = titulo;
+            subOptions = null;
+        }
+
+        //Metodos//
+        public void showInfo()
+        {
+
+            Console.WriteLine("Titulo: " + titulo);
+            Console.WriteLine("");
+            Console.WriteLine("OptionID = " + optionId);
+            Console.WriteLine("");
+            if (backOption != null)
+            {
+
+                Console.WriteLine("padre =" + backOption.titulo);
+
+            }
+            else
+            {
+                Console.WriteLine("padre = soy la raiz :)");
+
+            }
+
+            Console.WriteLine("");
+            if (subOptions != null)
+            {
+
+                Console.WriteLine("numero de hijos = " + subOptions.Length);
+
+            }
+            else
+            {
+                Console.WriteLine("numero de hijos = 0");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("");
+        }
+
+        public void agregarHijo(string nombreDeLaNuevaSubOpsion)
+        {
+
+            if (subOptions == null)
+            {
+                subOptions = new optionTree[1];
+                subOptions[0] = new optionTree(nombreDeLaNuevaSubOpsion, this, consoleMng.Id);
+                subOptions[0].backOption = this;
+            }
+            else
+            {
+                optionTree[] aux = new optionTree[subOptions.Length + 1];
+                for (int i = 0; i < subOptions.Length; i++)
+                {
+                    aux[i] = subOptions[i];
+                }
+                aux[aux.Length - 1] = new optionTree(nombreDeLaNuevaSubOpsion, this, consoleMng.Id);
+                aux[aux.Length - 1].BackOption = this;
+                this.subOptions = aux;
+            }
+
+        }
+
+        public void show()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("  " + titulo);
+            for (int l = 0; l < titulo.Length + 4; l++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            if (subOptions != null)
+            {
+                Console.WriteLine(" Opciones:");
+                string candidato = subOptions[0].titulo;
+                for (int i = 0; i < subOptions.Length; i++)
+                {
+                    if (candidato.Length <= subOptions[i].titulo.Length) { candidato = subOptions[i].titulo; }
+                }
+                if (candidato.Length < 10) { candidato = " Opciones:"; }
+                for (int j = 0; j < candidato.Length + 6; j++)
+                {
+                    Console.Write("-");
+                }
+                for (int k = 0; k < subOptions.Length; k++)
+                {
+                    if (subOptions[k].titulo == "Volver" || subOptions[k].titulo == "Salir")
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        Console.WriteLine("  *" + ") " + subOptions[k].titulo);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        Console.WriteLine("  " + subOptions[k].optionId + "" + ") " + subOptions[k].titulo);
+                    }
+                }
+            }
+        }
+
+        public void dejarComoExe()
+        {
+            this.exe = true;
+        }
+
+    }
+}
