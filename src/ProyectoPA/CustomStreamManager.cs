67b177d2f0d00aebe37ee34090601aea8153d1af
Sebastian Baixas <sbaixas@gmail.com>
2013-09-29 17:13:15 -0300

    :D no hay errores

--- src/ProyectoPA/CustomStreamManager.cs
+++ src/ProyectoPA/CustomStreamManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace ProyectoPA
 {
@@ -14,6 +15,31 @@ namespace ProyectoPA
             FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
             StreamWriter sw = new StreamWriter(fs);
             //
+        }
+
+        public static void Guardar_estado_FluxManager(FluxManager fmanager)
+        {
+            XmlSerializer mySerializer = new XmlSerializer(typeof(FluxManager));
+
+            StreamWriter myWriter = new StreamWriter("Flux.xml");
+            mySerializer.Serialize(myWriter, fmanager);
+            myWriter.Close();
+        }
+
+        public static FluxManager Cargar_estado_FluxManager()
+        {
+            FluxManager fmanager;
+  
+            XmlSerializer mySerializer = new XmlSerializer(typeof(FluxManager));
+
+            FileStream myFileStream = new FileStream("Flux.xml", FileMode.Open);
+ 
+            fmanager = (FluxManager)
+            mySerializer.Deserialize(myFileStream);
+
+            myFileStream.Close();
+
+            return fmanager;
         }
     }
 }
