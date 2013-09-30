d1651e343f3424dea65afec2c659533532251de7
Sebastian Baixas <sbaixas@gmail.com>
2013-09-30 07:15:14 -0300

    denuevo edite el gitignore version final

---
+++ src/bin/ProyectoPA/CustomStreamManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ProyectoPA
+{
+    class CustomStreamManager
+    {
+        private static IFormatter formatter = new BinaryFormatter();
+        public static void Guardar_Estado()
+        {
+            
+            Stream stream = new FileStream("Manager.sav", FileMode.Create);
+            formatter.Serialize(stream, Manager.Productos.ToArray());
+            Stream stream1 = new FileStream("Manager1.sav", FileMode.Create);
+            formatter.Serialize(stream1, Producto.IdCount);
+            Stream stream4 = new FileStream("Manager2.sav", FileMode.Create);
+            formatter.Serialize(stream4, Manager.Categorias.ToArray());
+            stream4.Close();
+            stream.Close();
+            stream1.Close();
+            Stream stream2 = new FileStream("Flux.sav", FileMode.Create);
+            formatter.Serialize(stream2, FluxManager.Registro_ingreso.ToArray());
+            Stream stream3 = new FileStream("Flux1.sav", FileMode.Create);
+            formatter.Serialize(stream3, FluxManager.Registro_venta.ToArray());
+            stream2.Close();
+            stream3.Close();
+        }
+
+        public static void Cargar_Estado()
+        {
+                Stream stream = new FileStream("Manager.sav", FileMode.Open);
+                Stream stream1 = new FileStream("Manager1.sav", FileMode.Open);
+                Stream stream4 = new FileStream("Manager2.sav", FileMode.Open);
+                string[] cats = (string[])formatter.Deserialize(stream4);
+                Producto[] prods = (Producto[])formatter.Deserialize(stream);
+                int countz = (int)formatter.Deserialize(stream1);
+                stream4.Close();
+                stream.Close();
+                stream1.Close();
+                Manager.Categorias = cats.ToList();
+                Manager.Productos = prods.ToList();
+                Producto.IdCount = countz;
+                Stream stream2 = new FileStream("Flux.sav", FileMode.Open);
+                Stream stream3 = new FileStream("Flux1.sav", FileMode.Open);
+                Registro[] regIng = (Registro[])formatter.Deserialize(stream2);
+                Registro[] regVen = (Registro[])formatter.Deserialize(stream3);
+                stream2.Close();
+                stream3.Close();
+                FluxManager.Registro_ingreso = regIng.ToList();
+                FluxManager.Registro_venta = regVen.ToList();
+        }
+    }
+}
---
+++ src/bin/ProyectoPA/TableMaker.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPA
+{
+    //bozzo//
+    class TableMaker
+    {
+        public TableMaker(string[] parametros, int[] formato, List<Producto> lista)
+        {
+
+
+            string[,] x = new string[parametros.Length, lista.Count];
+            for (int i = 0; i < lista.Count; i++)
+            {
+                for (int j = 0; j < parametros.Length; j++)
+                {
+                    if (j == 0)
+                    {
+                        x[j, i] = lista.ElementAt(i).Id + "";
+                    }
+                    if (j == 1)
+                    {
+                        x[j, i] = lista.ElementAt(i).Nombre + "";
+                    }
+                    if (j == 2)
+                    {
+                        x[j, i] = lista.ElementAt(i).Costo + "";
+                    }
+                    if (j == 3)
+                    {
+                        x[j, i] = lista.ElementAt(i).Tamaño + "";
+
+                    }
+                    if (j == 4)
+                    {
+                        x[j, i] = lista.ElementAt(i).Peso + "";
+
+                    }
+                    }
+
+                    for (int k = 0; k < parametros.Length; k++)
+                    {
+                        x[k, i] = darFormatoDeColumna(formato[i], parametros[i]);
+
+                        string[] output = new string[] { x[k, i] };
+                        TableMaker.ImprimirFila(output);
+                        Console.WriteLine("");
+                    }
+                }
+               
+            }
+        
+        static public string darFormatoDeColumna(int anchoDeColumna, string palabra)
+        {
+            string ret = palabra;
+            while (ret.Length < anchoDeColumna)
+            {
+                ret = ret + " ";
+                if (ret.Length < anchoDeColumna)
+                {
+                    ret = " " + ret;
+                }
+            }
+            return ret;
+        }
+        static public string darFormatoDeColumna(int anchoDeColumna, int palabra)
+        {
+            string ret;
+            ret = palabra + "";
+            while (ret.Length < anchoDeColumna)
+            {
+                ret = ret + " ";
+                if (ret.Length < anchoDeColumna)
+                {
+                    ret = " " + ret;
+                }
+            }
+            return ret;
+        }
+        static public string ImprimirFila(string[] columnas)
+        {
+            Console.Write(" |");
+            for (int i = 0; i < columnas.Length; i++)
+            {
+                Console.Write(columnas[i] + "|");
+            }
+            return null;
+        }
+        static public string ImprimirParametros(string[] columnas, int[] formato)
+        {
+            string linea = " ";
+            //devuelve el porte de la linea para no tener que calcularlo al final//
+
+            for (int i = 0; i < columnas.Length; i++)
+            {
+                columnas[i] = darFormatoDeColumna(formato[i], columnas[i]);
+
+            }
+
+            Console.WriteLine("");
+            int porteDeLalinea = 1 + columnas.Length;
+            for (int i = 0; i < columnas.Length; i++)
+            {
+                porteDeLalinea += columnas[i].Length;
+            }
+            Console.Write(" ");
+            for (int l = 0; l < porteDeLalinea; l++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("");
+
+            
+            Console.Write(" |");
+            for (int j = 0; j < columnas.Length; j++)
+            {
+                Console.Write(columnas[j] + "|");
+            }
+
+            Console.WriteLine("");
+            Console.Write(" ");
+           
+            for (int l = 0; l < porteDeLalinea; l++)
+            {
+               linea += "-";
+               Console.Write("-");
+            }
+            Console.WriteLine("");
+            
+            
+            return linea;
+        }
+    }
+}
